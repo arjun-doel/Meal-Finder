c37eaea1c175d901283f62056952dfa2c109816a
Arjun Doel <arjundoel@gmail.com>
2021-06-06 16:42:39 +0100

    add javascript

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 //* DOM Selectors
 
-const search = document.getElementById('search-btn')
+const search = document.getElementById('search-text')
+const foodCard = document.getElementById('food-card')
 
 
-search.addEventListener('click', e => {
-  
-})
+function searchText(e) {
+  const userText = e.target.value
+
+  if (userText.trim()){
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${userText}`)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+      });
+  }
+}
+
+
+
+//* Event Listners 
+search.addEventListener('keyup', searchText)
